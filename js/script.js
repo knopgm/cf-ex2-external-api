de512650d32830f987a44231b091be8754c10297
knopgm <knop.gm@gmail.com>
2022-03-15 21:15:45 +0100

    Add a loop to display the pokemon list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 let pokemonList = [
-  { name: "Bulbasaur", height: 7, type: ["monster", "grass"] },
-  { name: "Charmander", height: 6, type: ["monster", "dragon"] },
-  { name: "Caterpie", height: 3, type: ["bug"] },
+  { name: "Bulbasaur", height: 0.7, type: ["monster", "grass"] },
+  { name: "Charmander", height: 0.6, type: ["monster", "dragon"] },
+  { name: "Caterpie", height: 0.3, type: ["bug"] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+    `${pokemonList[i].name} (${pokemonList[i].height} height). / `
+  );
+}
