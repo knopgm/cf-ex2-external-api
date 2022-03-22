f1af161bea49b65da5203fd8142b20e498cfd951
knopgm <knop.gm@gmail.com>
2022-03-22 21:50:01 +0100

    Create a pokemon Repository into a IIFE and improve the list access with a foreach method

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, type: ["monster", "grass"] },
-  { name: "Charmander", height: 0.6, type: ["monster", "dragon"] },
-  { name: "Caterpie", height: 0.3, type: ["bug"] },
-];
+// pokemon repository wrapped into a IIFE to prevent accidentaly accessing to the global state:
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 0.6) {
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["monster", "grass"] },
+    { name: "Charmander", height: 0.6, type: ["monster", "dragon"] },
+    { name: "Caterpie", height: 0.3, type: ["bug"] },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemonName, height, type) {
+    pokemonList.push({ name: pokemonName, height: height, type: [type] });
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+//Create a function to print ANY pokemon list with its name and height:
+//The same function with foreach for readability:
+//PokemonList is a local variable inside the IIFE. Call getAll to have access to this list
+
+pokemonRepository.getAll().forEach((pokemon) => {
+  if (pokemon.height > 0.6) {
     document.write(
-      `<p>${pokemonList[i].name} (${pokemonList[i].height} height). - Wow, that's big! </p><br>`
+      `<p>${pokemon.name} (${pokemon.height} height). - Wow, that's big!!! </p><br>`
     );
   } else {
-    document.write(
-      `<p>${pokemonList[i].name} (${pokemonList[i].height} height). </p><br> `
-    );
+    document.write(`<p>${pokemon.name} (${pokemon.height} height). </p><br> `);
   }
-}
+});
